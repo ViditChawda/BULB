6d747bba8544642a52c6b968085864464ead544a
Acer <viditchawda301@gmail.com>
2022-02-19 17:59:01 +0530

    pics added

--- bulb.js
+++ bulb.js
@@ -1,12 +1,12 @@
 const bulb_image = document.getElementById('bulb');
 
 function on(){
-  bulb_image.src = "images/bulb_on.jpeg";
+  bulb_image.src = "bulb_on.jpeg";
   console.log(bulb_image)
 }
 
 function off(){
-    bulb_image.src = "images/bulb_off.jpeg";
+    bulb_image.src = "bulb_off.jpeg";
     console.log(bulb_image);
 }
 
